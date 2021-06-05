9a609939fd674532fd1f0764d5cae0799aee8742
Wallace Gonçalves <wallacecosme@gmail.com>
2021-06-05 12:16:18 -0300

    Userrepository, UserResouce, Zuul config

---
+++ hr-user/src/main/java/com/wg/hruser/resources/UserResouce.java
@@ -0,0 +1,36 @@
+package com.wg.hruser.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wg.hruser.entities.User;
+import com.wg.hruser.repositories.UserRepository;
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResouce {
+	
+	
+	@Autowired
+	private UserRepository repository;
+		
+		
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){		
+		User entity = repository.findById(id).get();		
+		return ResponseEntity.ok(entity);
+	}
+	
+	@GetMapping(value = "search")
+	public ResponseEntity<User> findByEmail(@RequestParam String email){		
+		User entity = repository.findByEmail(email);
+		return ResponseEntity.ok(entity);
+	}
+
+}
