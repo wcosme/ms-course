58a437d0770cf2132eaaa7d4f9a562843f052b90
Wallace Gonçalves <wallacecosme@gmail.com>
2021-08-22 19:00:52 -0300

    Alterando as configurações para o Docker

--- hr-worker/src/main/java/com/wg/rhworker/resources/WorkerResouce.java
+++ hr-worker/src/main/java/com/wg/rhworker/resources/WorkerResouce.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -24,9 +23,6 @@ public class WorkerResouce {
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerResouce.class);
 	
-	@Value("${test.config}")
-	private String testConfig;
-	
 	@Autowired
 	private Environment env;
 	
@@ -36,7 +32,7 @@ public class WorkerResouce {
 	
 	@GetMapping(value = "/configs")
 	public ResponseEntity<Void> getConfigs(){
-		logger.info("CONFIG = " +testConfig);
+		//logger.info("CONFIG = " +testConfig);
 		
 		return ResponseEntity.noContent().build();
 	}
@@ -52,7 +48,7 @@ public class WorkerResouce {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
 		
-		logger.info("PORTA: " + env.getProperty("local.server.port"));
+		logger.info("PORT: " + env.getProperty("local.server.port"));
 		
 		Worker entity = repository.findById(id).get();		
 		return ResponseEntity.ok(entity);
